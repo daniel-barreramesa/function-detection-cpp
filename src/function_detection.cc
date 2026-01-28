72599f5a5b3307bb830659ed18556a1a9c744e7d
daniel-barreramesa <alu0101742830@ull.edu.es>
2026-01-28 09:37:08 +0000

    v0.0.3

--- src/function_detection.cc
+++ src/function_detection.cc
@@ -1,18 +1,24 @@
 #include<iostream>
 #include<string>
 #include<cmath>
+#include "functions.h"
 
-void detect(std::string ecuation){
-    int parnum {0}; //the number of oppened parentheses
-    int parpos {0};
-    int lenght = ecuation.size(); //takes the lenght of the ecuation
-    for(int i{0}; i<lenght; ++i){ //use a for bucle to traverse the ecuation
-      if(ecuation[i]=='('){ //search parentheses
-        parnum++;
-        parpos = i; //saves the position of the last parentheses
-      }
-      if(ecuation[i]==')'){ //search parentheses
-        parnum++;
-      }
-    }
+
+
+std::string detect(std::string ecuation){
+  int lenght = ecuation.size();
+  for(int i{0}; i<lenght; ++i){ //search napierian logarithm
+    if(ecuation[i]== 'l' && ecuation[i+1]== 'n')
+      std::cout << "napierian logarithm \n";
+  
+    if(ecuation[i]== 's' && ecuation[i+1]== 'e' && ecuation[i+2]== 'n') //search sine
+      std::cout << "sine \n";
+  
+    if(ecuation[i]== 'c' && ecuation[i+1]== 'o' && ecuation[i+2]== 's')//search cosine
+      std::cout << "cosine \n";
+
+    if(ecuation[i]== 't' && ecuation[i+1]== 'g')//search tangent
+      std::cout << "tangent \n";
+  }
+  return "";
 }
---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+
+std::string detect(std::string ecuation);
+
+int detect_x(std::string ecuation);
+void solve_parentheses(std::string ecuation);
+void solve_functions(std::string ecuation);
+void solve_powers(std::string ecuation);
+void solve_products(std::string ecuation);
+void solve_sum(std::string ecuation);
+
